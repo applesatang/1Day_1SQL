ca2bcad2b73767f3ee9598ca9f9e2d5caada0419
applesatang <132911979+applesatang@users.noreply.github.com>
2024-04-02 23:23:37 +0900

    Time: 1475 ms (13.32%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
-FROM Activity A 
--- LEFT JOIN Activity B ON A.player_id = B.player_id
+FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date
+FROM Activity
+GROUP BY player_id) A 
 JOIN (SELECT * FROM Activity, (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R) B ON A.player_id = B.player_id
 WHERE DATEDIFF(B.event_date,A.event_date) = 1
+
