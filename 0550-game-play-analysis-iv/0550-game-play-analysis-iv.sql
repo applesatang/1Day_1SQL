ec6c97775dbb5ab84947a000a948f9f40036251d
applesatang <132911979+applesatang@users.noreply.github.com>
2024-04-02 23:56:24 +0900

    Time: 1184 ms (34.02%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,8 +1,20 @@
-#1안
-SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
-FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date
-      FROM Activity
+#1안(1755ms)
+-- SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
+-- FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date
+--       FROM Activity
+--       GROUP BY player_id) A 
+-- JOIN (SELECT * 
+--       FROM Activity, (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R) B ON A.player_id = B.player_id
+-- WHERE DATEDIFF(B.event_date,A.event_date) = 1
+
+#2안
+SELECT ROUND(((CASE WHEN DATEDIFF(B.event_date,A.event_date) = 1 
+                    THEN @sum:=@sum+1 ELSE @sum := @sum+0 END)/c.all_count),2) as fraction
+FROM (SELECT player_id,min(event_date) as event_date 
+      FROM Activity 
       GROUP BY player_id) A 
-JOIN (SELECT * 
-      FROM Activity, (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R) B ON A.player_id = B.player_id
-WHERE DATEDIFF(B.event_date,A.event_date) = 1
+LEFT JOIN Activity B ON A.player_id=B.player_id
+JOIN (SELECT @sum:=0) as R
+JOIN (SELECT COUNT(DISTINCT player_id)as all_count FROM Activity) C
+ORDER BY fraction DESC
+LIMIT 1
