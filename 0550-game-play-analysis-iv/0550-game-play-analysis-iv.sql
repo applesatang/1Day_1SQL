b290fb80b69632501dcdd2376ab59e82488e4af5
applesatang <132911979+applesatang@users.noreply.github.com>
2024-04-02 23:43:52 +0900

    Time: 1010 ms (60.97%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
-SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
-FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date
-FROM Activity
-GROUP BY player_id) A 
-JOIN (SELECT * FROM Activity, (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R) B ON A.player_id = B.player_id
-WHERE DATEDIFF(B.event_date,A.event_date) = 1
+-- SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
+-- FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date
+--       FROM Activity
+--       GROUP BY player_id) A 
+-- JOIN (SELECT * 
+--       FROM Activity, (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R) B ON A.player_id = B.player_id
+-- WHERE DATEDIFF(B.event_date,A.event_date) = 1
 
+SELECT ROUND(COUNT(DISTINCT A.player_id)/@all,2) AS fraction
+FROM (SELECT player_id,device_id,games_played,min(event_date) as event_date , @all
+      FROM Activity , (SELECT @all:= COUNT(DISTINCT player_id) FROM Activity) R
+      GROUP BY player_id) A 
+JOIN Activity B ON A.player_id = B.player_id
+WHERE DATEDIFF(B.event_date,A.event_date) = 1
